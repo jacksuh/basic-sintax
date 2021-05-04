6021a08a11c98a2745a04c33bedb8722020f7983
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-04 17:29:47 -0300

    basic sintax

---
+++ src/Programa.java
@@ -0,0 +1,11 @@
+public class Programa {
+    public static void main(String[] args){
+        System.out.println("ola Mundo");
+
+        int idade = 37;
+
+
+
+        System.out.println("a idade é "+ idade + ", parabéns!");
+    }
+}
