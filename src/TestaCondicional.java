e4ffb8e67448ab53ef2294886520c622ea28b4f6
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-04 20:15:59 -0300

    conditional

---
+++ src/TestaCondicional.java
@@ -0,0 +1,17 @@
+public class TestaCondicional {
+
+    public static void main(String[] args) {
+        System.out.println("Testando condicionais");
+        int idade = 20;
+        int quantidadePessoas = 3;
+
+        if(idade >= 18 || quantidadePessoas >= 2){
+            System.out.println("Você tem mais de 18 anos");
+            System.out.println("seja bem vindo");
+        }
+
+        else{
+                System.out.println("Você não pode entrar");
+            }
+        }
+    }
